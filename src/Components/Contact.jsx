7158a8a21a14799f4dfa8ad0fb25a9e3de6a5a51
divs94 <divyatlng0@gmail.com>
2023-08-01 14:56:45 +0530

    updated with details

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Carousel from 'react-bootstrap/Carousel';
-    import Tab from 'react-bootstrap/Tab';
+import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Table from 'react-bootstrap/Table';
 
@@ -9,154 +9,163 @@ export default function Contact() {
   return (
     <>
       <section>
-      
-    <Tabs
-      defaultActiveKey="contact"
-      id="justify-tab-example"
-      className="mt-3"
-      style={{padding:"0px 60px"}}
-      justify
-    >
-      <Tab eventKey="contact" title="Contact">
-      <div className="contact_body">
-          <div className="contact_wrapper">
-              <div className="contact_card">
-                <h3>Social Links</h3>
-                <ul>
-                  <li>
-                    <span>Email:</span>
-                    <span>
-                      <a
-                        target="_/blank"
-                        href="mailto:divyatlng0@gmail.com ? Cc= tlngdivya@gmail.com"
-                      >
-                        divyatlng0@gmail.com
-                      </a>
-                    </span>
-                  </li>
 
-                  <li>
-                    <span>Resume Pdf:</span>
-                    <p>
-                      <a
-                        target="_/blank"
-                        href="https://drive.google.com/file/d/16GUZ9m_39ZY4mEEwremwiC2Q9nTWCZgG/view?usp=sharing"
-                      >
-                        Divya's Resume
-                      </a>
-                    </p>
-                  </li>
+        <Tabs
+          defaultActiveKey="contact"
+          id="justify-tab-example"
+          className="mt-3"
+          style={{ padding: "0px 60px" }}
+          justify
+        >
+          <Tab eventKey="contact" title="Contact">
+            <div className="contact_body">
+              <div className="contact_wrapper">
+                <div className="contact_card">
+                  <h3>Social Links</h3>
+                  <ul>
+                    <li>
+                      <span>Email:</span>
+                      <span>
+                        <a
+                          target="_/blank"
+                          href="mailto:divyatlng0@gmail.com ? Cc= tlngdivya@gmail.com"
+                        >
+                          divyatlng0@gmail.com
+                        </a>
+                      </span>
+                    </li>
 
-                  <li>
-                    <span>Mobile:</span>
-                    <p>
-                      <a target="_/blank" href="tel:+917019898582">
-                        +91 701 989 8582
-                      </a>
-                    </p>
-                  </li>
-                  <li>
-                    <span>Github:</span>
-                    <span>
-                      <a target="_/blank" href="https://github.com/divs94">
-                        https://github.com/divs94
-                      </a>
-                    </span>
-                  </li>
+                    {/* <li>
+                      <span>Resume Pdf:</span>
+                      <p>
+                        <a
+                          target="_/blank"
+                          href="https://drive.google.com/file/d/16GUZ9m_39ZY4mEEwremwiC2Q9nTWCZgG/view?usp=sharing"
+                        >
+                          Divya's Resume
+                        </a>
+                      </p>
+                    </li> */}
 
-                  <li>
-                    <span>Linked In:</span>
-                    <span>
-                      <a
-                        target="_/blank"
-                        href="https://www.linkedin.com/in/divya-t-468642203"
-                      >
-                        https://linkedin.com/divs94
-                      </a>
-                    </span>
-                  </li>
+                    <li>
+                      <span>Mobile:</span>
+                      <p>
+                        <a target="_/blank" href="tel:+917019898582">
+                          +91 701 989 8582
+                        </a>
+                      </p>
+                    </li>
+                    <li>
+                      <span>Dribble:</span>
+                      <span>
+                        <a target="_/blank" href="https://dribbble.com/iam_divs">
+                          https://dribbble.com/iam_divs
+                        </a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>Behance:</span>
+                      <span>
+                        <a
+                          target="_/blank"
+                          href="https://www.behance.net/divyatailang"
+                        >
+                          https://behance.com/divs94
+                        </a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>Github:</span>
+                      <span>
+                        <a target="_/blank" href="https://github.com/divs94">
+                          https://github.com/divs94
+                        </a>
+                      </span>
+                    </li>
 
-                  <li>
-                    <span>Behance:</span>
-                    <span>
-                      <a
-                        target="_/blank"
-                        href="https://www.behance.net/divyatailang"
-                      >
-                        https://behance.com/divs94
-                      </a>
-                    </span>
-                  </li>
+                    <li>
+                      <span>Linked In:</span>
+                      <span>
+                        <a
+                          target="_/blank"
+                          href="https://www.linkedin.com/in/divya-t-468642203"
+                        >
+                          https://linkedin.com/divs94
+                        </a>
+                      </span>
+                    </li>
 
-                  <li>
-                    <span>Figma:</span>
-                    <span>
-                      <a
-                        target="_/blank"
-                        href="https://www.figma.com/@iam_divs"
-                      >
-                        https://figma.com.divs94
-                      </a>
-                    </span>
-                  </li>
 
-                  {/* https://stackoverflow.com/users/16797259/divya */}
 
-                  <li>
-                    <span>Stackoverflow:</span>
-                    <span>
-                      <a
-                        target="_/blank"
-                        href=" https://stackoverflow.com/users/16797259/divya "
-                      >
-                        https://stckovrflw.com/divs94
-                      </a>
-                    </span>
-                  </li>
-                </ul>
+                    <li>
+                      <span>Figma:</span>
+                      <span>
+                        <a
+                          target="_/blank"
+                          href="https://www.figma.com/@iam_divs"
+                        >
+                          https://figma.com.divs94
+                        </a>
+                      </span>
+                    </li>
+
+                    {/* https://stackoverflow.com/users/16797259/divya */}
+
+                    <li>
+                      <span>Stackoverflow:</span>
+                      <span>
+                        <a
+                          target="_/blank"
+                          href=" https://stackoverflow.com/users/16797259/divya "
+                        >
+                          https://stckovrflw.com/divs94
+                        </a>
+                      </span>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-      </Tab>
+          </Tab>
 
 
-      <Tab eventKey="service" title="Services">
-      <div className="contact_header">
-            <h3>What I Offer</h3>
-            <div>
-              <ol>
-                <li>Professional in eye catchy UI(User interface)</li>
-                <li>professional in UX(User Experience)</li>
-                <li>
-                  Responsible for Compatible Designs with DESKTOP, TAB and
-                  MOBILE
-                </li>
-                <li>Excellent problem-solving skills</li>
-                <li>Specializing in website Designs</li>
-                <li>Expertise Creating and Redesign websites</li>
-                <li>
-                  Experienced at troubleshooting websites and Redesign websites
-                </li>
-                <li>Proficient in MERN development and coded sites</li>
-                <li>
-                  Familiarity with Adobe Products (Adobe illustration ,Adobe XD,
-                  Web Premium CSS5)
-                </li>
-                <li>Guaranteed customer satisfaction</li>
-                <li>
-                  Provide project on given time but i prefer before time
-                  delivery
-                </li>
-                <li>Continued support after completion</li>
-              </ol>
+          <Tab eventKey="service" title="Services">
+            <div className="contact_header">
+              <h3>What I Offer</h3>
+              <div>
+                <ol>
+                  <li>Professional in eye catchy UI(User interface)</li>
+                  <li>professional in UX(User Experience)</li>
+                  <li>
+                    Responsible for Compatible Designs with DESKTOP, TAB and
+                    MOBILE
+                  </li>
+                  <li>Excellent problem-solving skills</li>
+                  <li>Specializing in website Designs</li>
+                  <li>Expertise Creating and Redesign websites</li>
+                  <li>
+                    Experienced at troubleshooting websites and Redesign websites
+                  </li>
+                  <li>Proficient in MERN development and coded sites</li>
+                  <li>
+                    Familiarity with Adobe Products (Adobe illustration ,Adobe XD,
+                    Web Premium CSS5)
+                  </li>
+                  <li>Guaranteed customer satisfaction</li>
+                  <li>
+                    Provide project on given time but i prefer before time
+                    delivery
+                  </li>
+                  <li>Continued support after completion</li>
+                </ol>
+              </div>
+
+
             </div>
+          </Tab>
 
- 
-          </div>
-      </Tab>
-      
-    </Tabs> 
- 
+        </Tabs>
+
       </section>
     </>
   );
--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -8,99 +8,122 @@ export default function Experience() {
         <div className="carousel_card">
           <h3>OaktreeConnect India PVT. LTD. </h3>
           <h5>
-            <div>Role :Full stack Developer</div>
-            <div>Jul 2022 – Current</div>
+            <div>Role : Full stack Developer</div>
+            <div>Jul 2022 – June 2023</div>
           </h5>
           <p>
-           
-              <hr />
-                Working on OaktreeConnect Web Application which provides Mental
-                Health treatment by online consultation with across the world’s
-                best Practitioners for people in United Kingdom (UK).
-              
-              <hr />
-                Working on React.js, Apollo Client, React MUI, JavaScript, for
-                Implementation of Front-End Development.
-              
-              <hr />
-                Working on Node.js, GraphQl api , NoSQL , Apollo Gateway
-                Implementation of Back-End Development .
-              
-              <hr />
-                <strong>Website :</strong>{" "}
-                <a target="_/blank" href="https://oaktreeconnect.co.uk/">
-                  https://oaktreeconnect.co.uk/
-                </a>
-              <hr />
 
-                <strong>Patient Portal :</strong>{" "}
-                <a target="_/blank" href="https://patient.oaktreeconnect.co.uk/signin">
-                https://patient.oaktreeconnect.co.uk/
-                </a>
-              <hr />
+            <hr />
+            Worked on OaktreeConnect Web Application which provides Mental
+            Health treatment by online consultation with across the world’s
+            best Practitioners for people in United Kingdom (UK).
 
-                <strong>Practitioner Portal :</strong>{" "}
-                <a target="_/blank" href="https://practitioner.oaktreeconnect.co.uk/signin">
-                https://practitioner.oaktreeconnect.co.uk/
-                </a>
-              
-              <hr />
-               Location: Birmingham,(United Kingdom) 
-            
+            <hr />
+            Worked on React.js, Apollo Client, React MUI, JavaScript, for
+            Implementation of Front-End Development.
+
+            <hr />
+            Worked on Node.js, GraphQl api , NoSQL , Apollo Gateway
+            Implementation of Back-End Development .
+
+            <hr />
+            Others: Worked on Strapi (CMS tool) for managing content, SEO Implementations for marketing , Redesigned Several parts .
+
+            <hr />
+            <strong>Website :</strong>{" "}
+            <a target="_/blank" href="https://oaktreeconnect.co.uk/">
+              https://oaktreeconnect.co.uk/
+            </a>
+            <hr />
+
+            <strong>Patient Portal :</strong>{" "}
+            <a target="_/blank" href="https://patient.oaktreeconnect.co.uk/signin">
+              https://patient.oaktreeconnect.co.uk/
+            </a>
+            <hr />
+
+            <strong>Practitioner Portal :</strong>{" "}
+            <a target="_/blank" href="https://practitioner.oaktreeconnect.co.uk/signin">
+              https://practitioner.oaktreeconnect.co.uk/
+            </a>
+
+            <hr />
+            Location: Birmingham,(United Kingdom)
+
+          </p>
+        </div>
+
+        <div className="carousel_card">
+          <h3>OSSTF Technologies PVT. LTD. </h3>
+          <h5>
+            <div>Role : UI/UX Designer</div>
+            <div>Apr 2022 – June 2022</div>
+          </h5>
+          <p>
+
+            <hr />
+            Worked on OSSTF Technologies PVT LTD, where i have Designed 2 Applications paralally.
+            one was mobile application, second was a web application.
+
+            <hr />
+            Worked on Figma, Canva, AdobeXD
+            <hr />
+            Location: Mumbai,(India)
+
           </p>
         </div>
 
         <div className="carousel_card">
           <h3>JUUBIX LTD (Global Techsprint Leadership Program) </h3>
           <h5>
-            <div>Role : Front-End Developer and Designer</div>
+            <div>Role : UI/UX Designer & Front-End Developer</div>
             <div>Oct 2021 – May 2022</div>
           </h5>
           <p>
-           
-              <hr />
-                Worked on , React.js JavaScript.and Figma. for Implementation of
-                Front-End Development of α-version website of JUUBIX{" "}
-              <hr />
-              
-                Worked in α-version website of JUUBIX as a Front-End Developer
-                and UI/UX Designer
-              
-              <hr />
-                JUUBIX is a Distributed Multi-Cloud based Omni-Channel for goods
-                and services - offering a SaaS AI matching tool for startups,
-                projects, expertise and finance.
-              <hr />
-               Location: London(United Kingdom)
-            
+
+            <hr />
+            Worked on , React.js JavaScript.and Figma. for Implementation of
+            Front-End Development of α-version website of JUUBIX{" "}
+            <hr />
+
+            Worked in α-version website of JUUBIX as a Front-End Developer
+            and UI/UX Designer
+
+            <hr />
+            JUUBIX is a Distributed Multi-Cloud based Omni-Channel for goods
+            and services - offering a SaaS AI matching tool for startups,
+            projects, expertise and finance.
+            <hr />
+            Location: London(United Kingdom)
+
           </p>
         </div>
 
         <div className="carousel_card">
           <h3>Search N Play Infotech </h3>
           <h5>
-            <div>Role :Front-End Developer and Designer</div>
+            <div>Role : Front-End Developer and Designer</div>
             <div>Jan 2021 – Feb 2022</div>
           </h5>
           <p>
-           
-              <hr />
-                Worked on HTML , CSS , React Bootstrap , JavaScript , React.js.
-                for Implementation of Front-End Development.
-              <hr />
-              
-                Working on Figma, Adobe XD, Canva, Slack for Implementation of
-                Front-End Design.
-              <hr />
-              Location: Jaipur ,Rajasthan
 
-              <hr />
-              <strong>Website :</strong>{" "}
-                <a target="_/blank" href="https://www.searchnplays.com/">
-                https://www.searchnplays.com/
-                </a>
+            <hr />
+            Worked on HTML , CSS , React Bootstrap , JavaScript , React.js.
+            for Implementation of Front-End Development.
+            <hr />
 
-            
+            Working on Figma, Adobe XD, Canva, Slack for Implementation of
+            Front-End Design.
+            <hr />
+            Location: Jaipur ,Rajasthan
+
+            <hr />
+            <strong>Website :</strong>{" "}
+            <a target="_/blank" href="https://www.searchnplays.com/">
+              https://www.searchnplays.com/
+            </a>
+
+
           </p>
         </div>
       </div>
